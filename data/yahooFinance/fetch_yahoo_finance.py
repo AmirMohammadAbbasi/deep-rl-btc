89b2bfdaaf4415df87120f2a0025b75ae0ed2c4b
abbasiamirumohammad <abbasiamirumohammad@gmail.com>
2023-08-28 12:46:37 +0330

    add config file

--- data/yahooFinance/fetch_yahoo_finance.py
+++ data/yahooFinance/fetch_yahoo_finance.py
@@ -1,32 +1,40 @@
+from datetime import date
 import numpy as np
 import pandas as pd
 
 # Data Source
 import yfinance as yf
 
-tickers = 'BTC-USD'
-# period = '1mo'
-interval = '1d'
 
-start="2019-06-02"
-end="2021-06-10"
+from configparser import ConfigParser
 
-my_tickers= yf.Ticker(tickers)
+# Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+tickerInfo = config_object["TICKERINFO"]
+
+tickers = tickerInfo['name']
+period = tickerInfo['period']
+interval = tickerInfo['interval']
+
+start = tickerInfo['fromDate']
+end = tickerInfo['toDate']
+
+my_tickers = yf.Ticker(tickers)
 historical = my_tickers.history(start=start, end=end, interval=interval)
-
 
 # Get data
 data = yf.download(tickers=tickers, start=start, end=end, interval=interval)
 print(data)
 
-from datetime import date
- 
+
 # Returns the current local date
 today = date.today()
 
 DF = pd.DataFrame(data)
- 
+
 # save the dataframe as a csv file
-path = "history/{}-{}-{}-{}.csv".format(tickers, interval, "period", today)
+path = "./history/{}-{}-{}--{}.csv".format(tickers, interval, start, end)
 print(path)
 DF.to_csv(path)
