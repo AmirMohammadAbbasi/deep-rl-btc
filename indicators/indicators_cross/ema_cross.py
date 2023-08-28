3d886a8609f107346b48e504f91aeb88fdbc38bb
abbasiamirumohammad <abbasiamirumohammad@gmail.com>
2023-08-28 13:20:14 +0330

    update ema

--- indicators/indicators_cross/ema_cross.py
+++ indicators/indicators_cross/ema_cross.py
@@ -4,28 +4,36 @@ import pandas_ta as pta
 import traceback
 import matplotlib.pyplot as plt
 
-FILENAME="./BTC-USD-1d-period-2023-08-27.csv"
+from configparser import ConfigParser
+
+# Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+tickerInfo = config_object["TICKERINFO"]
+FILENAME = tickerInfo["path"]
+
 df = None
 
 closeColumn = 'Close'
 dateColumn = 'Date'
 
-def calc_sma():
+def calc_ema():
     global df
     print(df.tail(2))
     print(df.shape)
     # print(list(df.columns.values))
     # print(df[closeColumn])
 
-    df['d_sma_10']=pta.sma(df[closeColumn], length = 10)
-    df['d_sma_20']=pta.sma(df[closeColumn], length = 20)
-    df['10_above_20'] = (df["d_sma_10"] >= df["d_sma_20"]).astype(int)
-    df['sma_10_20'] = df['10_above_20'].diff().astype('Int64')
-    print(df.tail(5))
+    df['d_ema_10']=pta.ema(df[closeColumn], length = 10)
+    df['d_ema_20']=pta.ema(df[closeColumn], length = 20)
+    df['10_above_20'] = (df["d_ema_10"] >= df["d_ema_20"]).astype(int)
+    df['ema_10_20'] = df['10_above_20'].diff().astype('Int64')
+    print(df.head(5))
     # print("Bullish crossovers")
-    # print(df.loc[df['sma_10_20'] == 1])
+    # print(df.loc[df['ema_10_20'] == 1])
     # print("Bearish crossovers")
-    # print(df.loc[df['sma_10_20'] == -1])
+    # print(df.loc[df['ema_10_20'] == -1])
 
 
 def plot_crossover():
@@ -35,14 +43,14 @@ def plot_crossover():
     plt.grid()
     plt.ylabel("Price")
     plt.plot(df[closeColumn], label = 'BTC', color = 'k',alpha = 0.6)
-    plt.plot(df['d_sma_10'], label = 'SMA 10',color = 'g')
-    plt.plot(df['d_sma_20'], label = 'SMA 20',color = 'b')
-    plt.plot(df[df["sma_10_20"] == 1].index, 
-         df['d_sma_10'][df["sma_10_20"] == 1], 
+    plt.plot(df['d_ema_10'], label = 'EMA 10',color = 'g')
+    plt.plot(df['d_ema_20'], label = 'EMA 20',color = 'b')
+    plt.plot(df[df["ema_10_20"] == 1].index, 
+         df['d_ema_10'][df["ema_10_20"] == 1], 
          "^", markersize = 10, color = 'g', label = 'BUY')
     
-    plt.plot(df[df["sma_10_20"] == -1].index, 
-         df['d_sma_10'][df["sma_10_20"] == -1], 
+    plt.plot(df[df["ema_10_20"] == -1].index, 
+         df['d_ema_10'][df["ema_10_20"] == -1], 
          "v", markersize = 10, color = 'r', label = 'SELL')
 
     plt.title('BTC Moving Averages (10, 20)')
@@ -66,8 +74,8 @@ def check_file():
 
 
 def save_csv(df):
-    df.drop('d_sma_10', inplace=True, axis=1)
-    df.drop('d_sma_20', inplace=True, axis=1)
+    df.drop('d_ema_10', inplace=True, axis=1)
+    df.drop('d_ema_20', inplace=True, axis=1)
     df.drop('10_above_20', inplace=True, axis=1)
     df.to_csv(FILENAME)
 
@@ -90,6 +98,6 @@ if __name__ == '__main__':
         print("File not found")
     if not read_eod_data():
         print("Error reading data")
-    calc_sma()
+    calc_ema()
     plot_crossover()
     save_csv(df)
